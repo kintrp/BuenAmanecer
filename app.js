0f6d580e12acc14f4894284f5e7b95c7d1188289
lax <phil.kintrup@googlemail.com>
2024-08-16 08:13:43 +0200

    pages

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ toggleButton.addEventListener('click', () => {
     // Wenn die Klasse active nicht bereits auf dem navbarLinks-Element vorhanden ist, wird sie hinzugefügt. 
     navbarLinks.classList.toggle('active');
 });
+
+
+
+function filterProducts(category) {
+    // Alle Produkt-Elemente abrufen
+    const productos = document.querySelectorAll('.product');
+    
+    // Über alle Produkte iterieren
+    productos.forEach(product => {
+      if (category === 'alle') {
+        // Alle Produkte anzeigen
+        product.style.display = 'flex';
+      } else if (product.classList.contains(category)) {
+        // Produkte der ausgewählten Kategorie anzeigen
+        product.style.display = 'flex';
+      } else {
+        // Andere Produkte ausblenden
+        product.style.display = 'none';
+      }
+    });
+  }
+  
